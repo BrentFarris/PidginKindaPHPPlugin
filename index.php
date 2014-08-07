a35e5f6a554d15f894e69fefc50cbf3132c93eab
Brent Farris <brent@farrisarts.com>
2014-08-06 18:29:39 -0700

    Using dictionary with delegates
    Testing out dictionaries rather than using a billion if statements... there will be a billion delegates ;D

--- index.php
+++ index.php
@@ -4,17 +4,33 @@ function ToMonospace($str) {
 	return '<font face="Courier 10 Pitch" size="1">' . $str . '</font>';
 }
 
-function Send($msg, $user='') {
-	if (empty($user))
-		echo htmlentities($msg);
+function Send($msg, $user='', $mono=false) {
+	if ($mono)
+		$msg = ToMonospace(htmlentities($msg));
 	else
-		echo $user . ' ' . htmlentities($msg);
+		$msg = htmlentities($msg);
+	
+	if (empty($user))
+		echo $msg;
+	else
+		echo $user . ' ' . $msg;
+}
+
+function StartsWith($haystack, $needle) {
+	return strpos($haystack, '/' . $needle . ' ') === 0;
 }
 
 $sender = $_GET['sender'];
-$message = $_GET['msg'];
+$message = $_GET['msg'] . ' ';	// Add space at end of string to check multiple commands
+								// that may start with the same string of characters
 
-if ($message == '/roll')
-	Send('rolled ' . mt_rand(1, 6), $sender);
-else if ($message == "/brent")
-	Send(ToMonospace(htmlentities(file_get_contents('brent.txt'))));
+$cmd = explode(' ', $message);
+$cmd = $cmd[0];
+
+$methods = array(
+	'/roll' => function($sender) { Send('rolled ' . mt_rand(1, 6), $sender); },
+	'/brent' => function($sender) { Send(file_get_contents('brent.txt'), '', true); }
+);
+
+if (array_key_exists($cmd, $methods))
+	$methods[$cmd]($sender);
