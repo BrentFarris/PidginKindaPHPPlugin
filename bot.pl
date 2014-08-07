acd93000f8a42bc9a506832b9da0fba9045040ee
Brent Farris <brent@farrisarts.com>
2014-08-07 15:10:05 -0700

    Removed line copy error "$"

--- bot.pl
+++ bot.pl
@@ -8,24 +8,13 @@ use LWP::Simple;
 	perl_api_version => 2,
 	name => "Brents Plugin",
 	version => "1.01",
-	summary => "A plugin that allows the reading and responding of messages in PHP via GET",
+	summary => "A plugin that allows the reading and responding of messages in PHP via GET.",
 	description => "This plugin allows you to write responses and consume text from ims and chats via PHP GET requests.",
 	author => "Brent A. Farris <brent\@beardedmangames.com",
 	url => "https://www.beardedmangames.com",
 	load => "plugin_load",
 	unload => "plugin_unload"
 );
-
-sub starts_with_cmd {
-	my $hs = HTML::Strip->new();
-	my $message = $hs->parse(shift . " ");
-	my $cmd = shift;
-	$hs->eof;
-
-	Purple::Debug::info("BRENTS PLUGIN", $message . "\n");
-
-	return $message =~ /^(\/$cmd\s)/;
-}
 
 sub check {
 	my $hs = HTML::Strip->new();
@@ -34,27 +23,20 @@ sub check {
 	my $me = shift;
 	my $sender = shift;
 	my $work = get("http://127.0.0.1/pidgin/index.php?me=".$me."&sender=".$sender."&msg=".$message);
-	
+
+	#Purple::Debug::info("BRENTS PLUGIN", $work . "\n");
+
 	if ($work ne "") {
 		$conv->get_im_data()->send($work);
 	}
-
-	#if (starts_with_cmd($message, "roll")) {
-	#	#$conv->get_im_data()->write("SENDER", "Rolling", 0, 0);
-	#	$conv->get_im_data()->send("Rolling");
-	#}
-
-	#if (starts_with_cmd($message, "google")) {
-	#	$conv->get_im_data()->send($loc);
-	#}
+	
+	#Purple::Debug::info("BRENTS PLUGIN", $message . "\n");
 }
 
 sub sent_im_msg {
 	my ($account, $receiver, $message) = @_;
 	my $conv = Purple::Conversation->new(1, $account, $receiver);
 	
-	#Purple::Debug::info("BRENTS PLUGIN", $message . "\n");
-
 	check($message, $conv, $account->get_username(), $account->get_username());
 }
 
@@ -89,30 +71,10 @@ sub plugin_load {
 	my $account_name = "baflink";
 	$account = Purple::Accounts::find($account_name, $protocol);
 
-	# Accounts
-	#$act_handle = Purple::Accounts::get_handle();
-	#Purple::Signal::connect($act_handle, "account-connecting", $plugin, \&account_connecting_cb, 0);
-
-	# Buddy List
-	#$blist = Purple::BuddyList::get_handle();
-	#Purple::Signal::connect($blist, "buddy-signed-on", $plugin, \&buddy_signed_on, 0);
-
-	# Connections
-	#$conn = Purple::Connections::get_handle();
-	#Purple::Signal::connect($conn, "signed-on", $plugin, \&signed_on, 0);
-
 	# Conversations
 	$conv = Purple::Conversations::get_handle();
 	Purple::Signal::connect($conv, "received-im-msg", $plugin, \&received_im_msg, "received im message");
 	Purple::Signal::connect($conv, "received-chat-msg", $plugin, \&received_chat_msg, "received chat message");
 	Purple::Signal::connect($conv, "sent-im-msg", $plugin, \&sent_im_msg, "received im message");
 	Purple::Signal::connect($conv, "sent-chat-msg", $plugin, \&sent_chat_msg, "received chat message");
-
-	# Here we send messages to the conversation
-	#print "Testing Purple::Conversation::IM::send()...\n";
-	#$im->send("Message Test.");
-	#print "Testing Purple::Conversation::IM::write()...\n";
-	#$conv2->get_im_data()->write("SENDER", "<b>Message</b> Test.", 0, 0);
-
-	#Purple::Debug::info("BRENTS PLUGIN", "Debugging stuff.\n");
 }
