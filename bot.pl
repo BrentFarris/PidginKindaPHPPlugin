79730890373f2ad0d9113d47afd1c93a6a70c962
Brent Farris <brent@farrisarts.com>
2014-08-07 17:31:48 -0700

    Fix for hiding php errors, warnings and notices

--- bot.pl
+++ bot.pl
@@ -7,7 +7,7 @@ use LWP::Simple;
 %PLUGIN_INFO = (
 	perl_api_version => 2,
 	name => "Brents Plugin",
-	version => "1.01",
+	version => "1.02",
 	summary => "A plugin that allows the reading and responding of messages in PHP via GET.",
 	description => "This plugin allows you to write responses and consume text from ims and chats via PHP GET requests.",
 	author => "Brent A. Farris <brent\@beardedmangames.com",
@@ -24,10 +24,17 @@ sub check {
 	my $sender = shift;
 	my $work = get("http://127.0.0.1/pidgin/index.php?me=".$me."&sender=".$sender."&msg=".$message);
 
-	#Purple::Debug::info("BRENTS PLUGIN", $work . "\n");
+	#Purple::Debug::info("BRENTS PLUGIN", "me ".$me."\n");
+	#Purple::Debug::info("BRENTS PLUGIN", "sender ".$sender."\n");
+	#Purple::Debug::info("BRENTS PLUGIN", "message ".$message."\n");
+	#Purple::Debug::info("BRENTS PLUGIN", "work ".$work."\n");
 
-	if ($work ne "") {
-		$conv->get_im_data()->send($work);
+	if ($work ne "" && $work !~ /^([wW]arning:)/ && $work !~ /^([fF]atal [eE]rror:)/ && $work !~ /^([pP]arse [eE]rror:)/ && $work !~ /^([nN]otice:)/) {
+		if (defined $conv->get_im_data()) {
+			$conv->get_im_data()->send($work);
+		} elsif (defined $conv->get_chat_data()) {
+			$conv->get_chat_data()->send($work);
+		}
 	}
 	
 	#Purple::Debug::info("BRENTS PLUGIN", $message . "\n");
@@ -42,7 +49,9 @@ sub sent_im_msg {
 
 sub received_im_msg {
 	my ($account, $sender, $message, $conv, $flags) = @_;
-	check($message, $conv, $account->get_username(), $sender);
+	if ($account->get_username() ne $sender) {
+		check($message, $conv, $account->get_username(), $sender);
+	}
 }
 
 sub sent_chat_msg {
@@ -53,7 +62,9 @@ sub sent_chat_msg {
 
 sub received_chat_msg {
 	my ($account, $sender, $message, $conv, $flags) = @_;
-	check($message, $conv, $account->get_username(), $sender);
+	if ($account->get_username() ne $sender) {
+		check($message, $conv, $account->get_username(), $sender);
+	}
 }
 
 sub plugin_init {
